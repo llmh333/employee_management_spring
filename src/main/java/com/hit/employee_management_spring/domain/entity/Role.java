acccb48ed490e9cf6e3566d56cbdcf79add7e617
LeMinHieu <lminhhieu2507@gmail.com>
2025-09-13 13:24:31 +0700

    feat: implement entity in db

---
+++ src/main/java/com/hit/employee_management_spring/domain/entity/Role.java
@@ -0,0 +1,24 @@
+package com.hit.employee_management_spring.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
